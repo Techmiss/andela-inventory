f07ddcc810427a9fdbe9723774e9e1d9b1fbd190
Techmiiss <buzor.mel@gmail.com>
2016-05-26 00:20:16 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.listen(1994);
+console.log('Server listening on port 1994');
