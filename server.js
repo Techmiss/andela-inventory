34f6331372edd15b1203ebb5145f6ed5e3b48863
Techmiiss <buzor.mel@gmail.com>
2016-05-26 01:21:01 +0100

    Add Homepage

--- server.js
+++ server.js
@@ -7,5 +7,9 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.get('/add', function(req, res) {
+	res.render('add');
+});
+
 app.listen(1994);
 console.log('Server listening on port 1994');
