d5bf6481a8392abe832ef64f6071a9b182b8fd28
Techmiiss <buzor.mel@gmail.com>
2016-05-26 21:33:41 +0100

    Connecting to firebase

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
-var app = express();
 var formidable = require('formidable');
+var Firebase = require('firebase');
+
+var firebase = new Firebase("https://andela-inventory-web.firebaseio.com/");
+var app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -15,10 +19,30 @@ app.get('/add', function(req, res) {
 app.post('/add',function (req, res){
 	var form = formidable.IncomingForm();
 	form.parse(req, function (err, fields, files) {
+		var inventoryItems = firebase.child('items');
+		inventoryItems.push(fields);
+	});
+	res.redirect('/');
+});
+app.get('/report', function(req, res) {
+	res.render('report');
+});
+app.post('/report',function (req, res){
+	var form = formidable.IncomingForm();
+	form.parse(req, function (err, fields, files) {
 		console.log(fields);
 	});
 	res.redirect('/');
 });
-
+app.get('/found', function(req, res) {
+	res.render('found');
+});
+app.post('/found',function (req, res){
+	var form = formidable.IncomingForm();
+	form.parse(req, function (err, fields, files) {
+		console.log(fields);
+	});
+	res.redirect('/');
+});
 app.listen(1994);
 console.log('Server listening on port 1994');
